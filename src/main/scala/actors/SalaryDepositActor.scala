e62a028f447f6d3b67b96ca62ddfdc1a5d381065
anuj1207 <anuj.saxena@knoldus.in>
2017-03-24 17:07:52 +0530

    fault tolerance added

--- src/main/scala/actors/SalaryDepositActor.scala
+++ src/main/scala/actors/SalaryDepositActor.scala
@@ -3,12 +3,19 @@ package actors
 import akka.actor.{Actor, ActorLogging, ActorRef}
 import models.{Biller, Salary}
 import akka.pattern.ask
+import akka.actor.OneForOneStrategy
+import akka.actor.SupervisorStrategy._
 import akka.util.Timeout
 
 import scala.concurrent.duration._
 import scala.collection.mutable.ListBuffer
 
 class SalaryDepositActor(dbRef: ActorRef) extends Actor with ActorLogging{
+
+  override val supervisorStrategy =
+    OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1 minute) {
+      case _: Exception => Restart
+    }
 
   implicit val timeout = Timeout(1000 seconds)
   override def receive = {
